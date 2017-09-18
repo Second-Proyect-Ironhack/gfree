f77e0daaac677c8864d5af56a76df7a6c3b28ca9
Jimena Martin <jimena.mg@hotmail.com>
2017-09-18 09:14:39 +0200

    Errores refactorizados, rutas ananidas

--- awesome-project/app.js
+++ awesome-project/app.js
@@ -6,10 +6,13 @@ const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const layouts      = require('express-ejs-layouts');
 const mongoose     = require('mongoose');
-
+const errors = require('./config/errors')
 
 mongoose.connect('mongodb://localhost/awesome-project');
 const authRoutes = require('./routes/auth');
+const index = require('./routes/index');
+const places = require('./routes/places')
+const products = require('./routes/products')
 const app = express();
 
 // view engine setup
@@ -27,26 +30,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
 
-const index = require('./routes/index');
+
 app.use('/', index);
 app.use('/', authRoutes);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/', products);
+app.use('/', places);
+app.use(errors)
 
 module.exports = app;
