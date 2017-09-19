856165f28dd57bf32d8ca469103de07aaed3130a
Jimena Martin <jimena.mg@hotmail.com>
2017-09-19 12:04:04 +0200

    Borrado repeticiones de app

--- app.js
+++ app.js
@@ -69,19 +69,8 @@ require('./passport/local');
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.use(passport.initialize())
-app.use(passport.session())
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
