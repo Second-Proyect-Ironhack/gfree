2c8a33220b1b0b0eb170a51ff9e116065200790c
Jimena Martin <jimena.mg@hotmail.com>
2017-09-22 12:50:40 +0200

    favorites

--- public/javascripts/favorite-place.js
+++ public/javascripts/favorite-place.js
@@ -4,6 +4,7 @@ $("#love").on("click", function(e){
   e.preventDefault()
   const placeID = {id : $("#love").attr("data-value")}
   addFavorite(placeID)
+  $("#love").attr("href", "#")
 })
 
 function addFavorite(id){
